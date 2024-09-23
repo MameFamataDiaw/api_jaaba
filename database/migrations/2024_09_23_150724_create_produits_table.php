28f78e101a4994a3e6f0a325deb40161a738d8d4
Fams-tech <diawmamefama@gmail.com>
2024-09-23 17:27:08 +0000

    Ajout des fichiers dans la branche dev

---
+++ database/migrations/2024_09_23_150724_create_produits_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id();
+            $table->string('libelle',30);
+            $table->string('description',100);
+            $table->integer('prix');
+            $table->integer('quantite');
+            $table->string('photo')->nullable();
+            $table->foreignId('categorie_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produits');
+    }
+};
---
+++ database/migrations/2024_09_23_151446_create_commandes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('montant');
+            $table->timestamp('date')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->enum('statut',['en cours','termine','annule'])->default('en cours');
+            $table->foreignId('client_id')->references('id')->on('clients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commandes');
+    }
+};
